4cadc33a29a8ea0ba41c9f02b1c5070bb7e3a475
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-06-04 12:45:25 +0300

    Update verbose.py

--- core/commands/verbose.py
+++ core/commands/verbose.py
@@ -36,9 +36,9 @@ def execute(shell, cmd):
         sw = splitted[1].lower()
         if sw == "1" or sw == "true" or sw == "on":
             shell.verbose = True
-            shell.print_status("Verbosity: %s" % ("on" if shell.verbose else "off"))
+            shell.print_info("Verbosity: %s" % ("on" if shell.verbose else "off"))
         if sw == "0" or sw == "false" or sw == "off":
             shell.verbose = False
-            shell.print_status("Verbosity: %s" % ("on" if shell.verbose else "off"))
+            shell.print_info("Verbosity: %s" % ("on" if shell.verbose else "off"))
     else:
         help(shell)
